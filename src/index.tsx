aa82eefe783b4409cbb8152ba54905e8b1680b20
yj-oh <holidayoh99@gmail.com>
2021-06-01 02:21:46 +0900

    [추가] index.tsx | store 등록

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './store';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
